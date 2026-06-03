bd0568558e96075fc985bb1592de178e0800c06a
Alexander Escobar <lexand.esc@gmail.com>
2026-06-02 23:45:09 -0500

    feat: add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analitycs } from "@vercel/analytics/react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -26,6 +27,7 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+        <Analitycs />
       </body>
     </html>
   );
